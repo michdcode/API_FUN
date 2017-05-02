5b5ec3aa54587d9be3f6a50b42312bbb2f02bf5b
michdcode <michd@alumni.stanford.edu>
2017-05-02 14:35:14 -0700

    Works with data, not variables

---
+++ Find_A_Restaurant.py
@@ -0,0 +1,58 @@
+from geocode import get_geocode_location
+import json
+import httplib2
+from datetime import date
+
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+foursquare_client_id = [os.environ'fs_client_id']
+foursquare_client_secret = [os.environ'fs_client_secret']
+
+
+def findARestaurant(meal_type, location):
+    #1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
+    lat_long = get_geocode_location(location)
+    lat_long_str = str(lat_long)
+    print (lat_long_str)
+    #2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
+    #HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+    current_date = []
+    today = date.today()
+    current_date.append(today)
+    date_str = str(current_date[0]).replace("-", "")
+    print date_str
+    meal_str = meal_type.replace(" ", "+")
+    # foursquareurl = ('https://api.foursquare.com/v2/venues/search?client_id=\
+    #                   foursquare_client_id&client_secret=foursquare_client_secret\
+    #                   &v=date_str&ll=lat_long_str&query=meal_str')
+    # foursquare_url = ('https://api.foursquare.com/v2/venues/search?ll=%s&client_id=%s&client_secret=%s&v=%s'% (lat_long_str, foursquare_client_id, foursquare_client_secret, date_str))
+    
+    foursquare_url2 = ('https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=""&client_secret=""&v=20170502')
+    h = httplib2.Http()
+    response, content = h.request(foursquare_url2, 'GET')
+    foursquare_result = json.loads(content)
+    print foursquare_result
+
+
+
+
+    #3. Grab the first restaurant
+    #4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+    #5. Grab the first image
+    #6. If no image is available, insert default a image url
+    #7. Return a dictionary containing the restaurant name, address, and image url
+
+
+if __name__ == '__main__':
+    findARestaurant("Pizza", "Tokyo, Japan")
+    findARestaurant("Tacos", "Jakarta, Indonesia")
+    findARestaurant("Tapas", "Maputo, Mozambique")
+    findARestaurant("Falafel", "Cairo, Egypt")
+    findARestaurant("Spaghetti", "New Delhi, India")
+    findARestaurant("Cappuccino", "Geneva, Switzerland")
+    findARestaurant("Sushi", "Los Angeles, California")
+    findARestaurant("Steak", "La Paz, Bolivia")
+findARestaurant("Gyros", "Sydney Australia")
